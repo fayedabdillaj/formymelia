6b2765bbf94b1a104a37292dbb275118f29b2470
fayedabdillaj <59002005+fayedabdillaj@users.noreply.github.com>
2026-02-14 17:53:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ("Roses are red, sky is blue, My love is true, and it's only for you, Melia").split('')
+    const titles = ("If love is a journey, I want to go far as long as it’s with you, Melia").split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
